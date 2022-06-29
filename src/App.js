f28a730999f2ba0a842125b347c58524fbf2fce1
biniyam112 <biniyamdemissew112@gmail.com>
2022-06-29 16:23:05 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import './App.css';
+import "./App.css";
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
-import { Header } from './components/header/header';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Header } from "./components/header/header";
 import { HomePage } from "./home/home_page";
-
-
-
 
 function App() {
   return (
-  <>
-  <Router>
-  <Header/>
-    <Routes>
-
-      <Route exact path="/" Component={HomePage} /> 
-    </Routes>
-</Router>  
-</>
-  )
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" Component={HomePage} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
-export default App
+export default App;
